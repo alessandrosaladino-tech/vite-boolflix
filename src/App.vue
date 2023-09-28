ce8df92278edda5882aee483c2b100f5589a6b65
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-28 13:01:44 +0200

    Add
    Some base script in store.js and app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <script>
-import {store} from "./store.js"
+import { store } from "./store.js"
 export default {
-   data(){
+  data() {
     return {
       store
     }
-   }
+  },
+
+  methods: {
+    addQuery() {
+      const urlMovieComplete = this.store.movieUrl + `&query=${this.store.query}`
+      this.state.fetchFilms(urlMovieComplete)
+    }
+  }
 }
 </script>
 
 <template>
- 
+  
+  <div>
+
+    <input v-model="this.store.query" type="text">
+    <button>trova</button>
+
+  </div>
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,23 @@ import {reactive} from "vue";
 import axios from "axios"
 
 export const store = reactive({
-    
+    movieUrl: "https://api.themoviedb.org/3/search/movie?api_key=811cc1d70d0da0842f58ea6aad4434ed",
+    myApiKey: "811cc1d70d0da0842f58ea6aad4434ed",
+    query: "",
+    films: "",
+
+
+    fetchFilms(url){
+
+        axios 
+        .get(this.url)
+        .then(response =>{
+            this.films = response.data
+            .console.log(this.films);
+        })
+        .catch(error =>{
+            console.error(error);
+        })
+    }
+
 })
