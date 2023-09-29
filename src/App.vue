44e0efd4521437594b14961b66cc7bd3c0f72978
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-29 10:15:23 +0200

    Add
    fix img

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ export default {
           <p>{{ film.original_title }}</p>
           <img
             :src="film.original_language == 'en' ? 'https://flagsapi.com/GB/shiny/64.png' : 'https://flagsapi.com/' + film.original_language.toUpperCase().split('_')[0] + '/shiny/64.png'"
-            @error="$event.target.src = 'https://cdn3.iconfinder.com/data/icons/faticons/32/globe-01-512.png'"  :alt="film.original_language"/> 
+            @error="$event.target.src = 'https://cdn2.iconfinder.com/data/icons/calin-support/32/-_52_world_support-1024.png'"  :alt="film.original_language"/> 
           
           <p>{{ film.vote_average }}</p>
 
