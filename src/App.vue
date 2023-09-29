3af0f6d5d8782020d417f4d35c2f5ea311d2b680
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-29 09:28:57 +0200

    Add
    API for language img

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,11 @@ export default {
       <ul>
         <li v-for="film in this.store.films">
           <p>{{ film.title }}</p>
-          <p>{{ film.original_title}}</p>
-          <p>{{ film.original_language }}</p>
+          <p>{{ film.original_title }}</p>
+          <img
+            :src="film.original_language == 'en' ? 'https://flagsapi.com/GB/shiny/64.png' : 'https://flagsapi.com/' + film.original_language.toUpperCase().split('_')[0] + '/shiny/64.png'"
+            @error="$event.target.src = 'https://cdn3.iconfinder.com/data/icons/faticons/32/globe-01-512.png'"  :alt="film.original_language"/> 
+          
           <p>{{ film.vote_average }}</p>
 
         </li>
