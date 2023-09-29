ad1a354239c2e36f4855a44a5c649d6d6b59864a
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-29 10:38:52 +0200

    Add
    tv series

--- src/store.js
+++ src/store.js
@@ -3,9 +3,11 @@ import axios from "axios"
 
 export const store = reactive({
     movieUrl: "https://api.themoviedb.org/3/search/movie?api_key=811cc1d70d0da0842f58ea6aad4434ed&query=",
+    tv_url: "https://api.themoviedb.org/3/search/tv?api_key=811cc1d70d0da0842f58ea6aad4434ed&query=",
     myApiKey: "811cc1d70d0da0842f58ea6aad4434ed",
     query: "",
     films: "",
+    tvs: "",
     completeUrl: "",
 
 
@@ -16,6 +18,18 @@ export const store = reactive({
         .then(response =>{
             this.films = response.data.results
             console.log(this.films);
+        })
+        .catch(error =>{
+            console.error(error);
+        })
+    },
+
+    fetchTv(url){
+        axios 
+        .get(url)
+        .then(response =>{
+            this.tvs = response.data.results
+            console.log(this.tvs);
         })
         .catch(error =>{
             console.error(error);
